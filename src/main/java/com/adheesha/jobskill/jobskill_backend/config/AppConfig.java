85b3d07af0a5747e8cc30a4d2c12d50fa219d20f
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-07-31 20:07:39 +0530

    add externalApiKey, externalApiUrl in AppConfig

--- src/main/java/com/adheesha/jobskill/jobskill_backend/config/AppConfig.java
+++ src/main/java/com/adheesha/jobskill/jobskill_backend/config/AppConfig.java
@@ -1,16 +1,33 @@
 package com.adheesha.jobskill.jobskill_backend.config;
 
+import com.adheesha.jobskill.jobskill_backend.dto.ApplicationDto;
 import com.adheesha.jobskill.jobskill_backend.dto.JobSeekerDto;
 import com.adheesha.jobskill.jobskill_backend.dto.TrainerDto;
+import com.adheesha.jobskill.jobskill_backend.entity.Application;
 import com.adheesha.jobskill.jobskill_backend.entity.JobSeeker;
 import com.adheesha.jobskill.jobskill_backend.entity.Trainer;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Value("${external.api.key}")
+    private String externalApiKey;
+
+    @Value("${external.api.url}")
+    private String externalApiUrl;
+
+    public String getExternalApiKey() {
+        return externalApiKey;
+    }
+
+    public String getExternalApiUrl() {
+        return externalApiUrl;
+    }
 
     @Bean
     public ModelMapper modelMapper() {
@@ -47,6 +64,10 @@ public class AppConfig {
                 skip(destination.getUser());
             }
         });
+
+        // Application → ApplicationDto (skip resumeUrl)
+        modelMapper.typeMap(Application.class, ApplicationDto.class)
+                .addMappings(mapper -> mapper.skip(ApplicationDto::setResumeUrl));
 
 
         return new ModelMapper();
