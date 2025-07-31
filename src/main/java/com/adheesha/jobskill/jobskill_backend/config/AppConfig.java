bd9c5104b7ce3b7a281862fef10ade519819de55
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-07-31 14:26:09 +0530

    add modelMapper addMappings in AppConfig

--- src/main/java/com/adheesha/jobskill/jobskill_backend/config/AppConfig.java
+++ src/main/java/com/adheesha/jobskill/jobskill_backend/config/AppConfig.java
@@ -1,6 +1,11 @@
 package com.adheesha.jobskill.jobskill_backend.config;
 
+import com.adheesha.jobskill.jobskill_backend.dto.JobSeekerDto;
+import com.adheesha.jobskill.jobskill_backend.dto.TrainerDto;
+import com.adheesha.jobskill.jobskill_backend.entity.JobSeeker;
+import com.adheesha.jobskill.jobskill_backend.entity.Trainer;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +14,41 @@ public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        // Custom mapping: JobSeeker → JobSeekerDto
+        modelMapper.addMappings(new PropertyMap<JobSeeker, JobSeekerDto>() {
+            @Override
+            protected void configure() {
+                map().setUserId(source.getUser().getUserId());
+            }
+        });
+
+        // Optional: JobSeekerDto → JobSeeker (skip user; you manually set in service)
+        modelMapper.addMappings(new PropertyMap<JobSeekerDto, JobSeeker>() {
+            @Override
+            protected void configure() {
+                skip(destination.getUser()); // You will manually set user later
+            }
+        });
+
+        // Trainer → TrainerDto
+        modelMapper.addMappings(new PropertyMap<Trainer, TrainerDto>() {
+            @Override
+            protected void configure() {
+                map().setUserId(source.getUser().getUserId());
+            }
+        });
+
+        // TrainerDto → Trainer
+        modelMapper.addMappings(new PropertyMap<TrainerDto, Trainer>() {
+            @Override
+            protected void configure() {
+                skip(destination.getUser());
+            }
+        });
+
+
         return new ModelMapper();
     }
 }
