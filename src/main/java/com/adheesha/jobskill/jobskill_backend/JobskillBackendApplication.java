f12b6735b0b8ce44b5e0fbb8e992cc0afef7ea00
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-07-31 05:55:47 +0530

    Initial commit

---
+++ src/main/java/com/adheesha/jobskill/jobskill_backend/JobskillBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adheesha.jobskill.jobskill_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobskillBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobskillBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adheesha/jobskill/jobskill_backend/JobskillBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adheesha.jobskill.jobskill_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobskillBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
